c16d1c6a116a27840dfec1d43e0b700f0910968d
Vikash Kumar Singh <68193781+csegeek@users.noreply.github.com>
2022-07-08 04:13:55 +0530

    Update ProductMetadataApplication.java

--- src/main/java/com/example/amazon_clone_backend/ProductMetadataApplication.java
+++ src/main/java/com/example/amazon_clone_backend/ProductMetadataApplication.java
@@ -19,7 +19,7 @@ public class ProductMetadataApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+				registry.addMapping("/**").allowedOrigins("https://vik-amazon-clone-frontend.herokuapp.com");
 			}
 		};
 	}
